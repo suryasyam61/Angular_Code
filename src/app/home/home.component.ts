d0b1ecec8566a2c3f2b7e8f5cfc60afb77098f75
Syam <88177289+suryasyam61@users.noreply.github.com>
2022-02-17 17:17:55 +0530

    Update home.component.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,22 +6,25 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
-  zipCode:any
+export class HomeComponent implements OnInit {zipCode!:string
   data:any;
+  key: string = 'Name';
+  myItem!: string;
   zcode!: number;
   url = 'http://api.openweathermap.org/data/2.5/weather?id=';
-  ccond!: number;      temp!: number;      temp_max!: number;      temp_min!: number;
+  ccond!: string;      temp!: number;      temp_max!: number;      temp_min!: number;
   ngOnInit(): void {
       
   }
+  sun=false; snow=false; rain=false; clouds=false;
   constructor(private http: HttpClient) {}
 
-        setZip(zipcode:any) {
+    setZip(zipcode:any) {
         this.zcode = zipcode;
           localStorage.setItem('Code ', zipcode);
           this.getData()
         }
+
     getData() {
         this.http.get(this.url + this.zcode+'&appid=5a4b2d457ecbef9eb2a71e480b947604').subscribe((res) => {
           this.data = res;
@@ -29,7 +32,19 @@ export class HomeComponent implements OnInit {
           this.temp = this.data.main.temp;
           this.temp_max = this.data.main.temp_max;
           this.temp_min = this.data.main.temp_min;
-          console.log(this.ccond + '   ' + this.temp  + '   ' +this.temp_min + '   ' +this.temp_max );        
+          console.log(this.ccond + '   ' + this.temp  + '   ' +this.temp_min + '   ' +this.temp_max );
+          if(this.ccond=="Snow"){
+            this.snow=!this.snow
+          }
+          else if(this.ccond=="Rain"){
+            this.rain= !this.rain
+          }
+          else if(this.ccond=="Clouds"){
+            this.clouds=!this.clouds
+          }
+          else
+          this.sun = !this.sun
     });    
   }
 }
+
